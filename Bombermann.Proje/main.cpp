c6bc9d707b58fbf49cf2f13f5c3e349da3212dee
muhammed <muhammedebraryel@gmail.com>
2026-05-05 21:17:13 +0300

    Harita çizildi ve oyuncu hareketi eklendi fakat duvarlara çarpma eklenemedi

--- Bombermann.Proje/main.cpp
+++ Bombermann.Proje/main.cpp
@@ -1,13 +1,91 @@
-#include <stdio.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Keyboard.hpp>
-#include <SFML/System/Clock.hpp>
 #include <optional>
 #include <vector>
 
-int main()
-{
+// Harita ayarlarý
+const unsigned int satir_sayisi = 9;
+const unsigned int sutun_sayisi = 13;
+const float kare_boyutu = 64.f;
+
+enum HucreTipi {
+    BOS = 0,
+    KIRILAMAZ_DUVAR = 1,
+    KIRILABILIR_KUTU = 2
+};
+
+int main() {
+    // pencere
+    sf::RenderWindow pencere(sf::VideoMode(sf::Vector2u(sutun_sayisi * kare_boyutu, satir_sayisi * kare_boyutu)), "Bomberman");
+    pencere.setFramerateLimit(60);
+
+    // harita
+    int harita[satir_sayisi][sutun_sayisi] = {
+        {0, 0, 0, 2, 2, 0, 2, 0, 2, 2, 0, 0, 0},
+        {0, 1, 2, 1, 2, 1, 0, 1, 2, 1, 2, 1, 0},
+        {2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2},
+        {0, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 0},
+        {2, 0, 2, 0, 2, 2, 0, 2, 2, 0, 2, 0, 2},
+        {0, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 0},
+        {2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2},
+        {0, 1, 2, 1, 2, 1, 0, 1, 2, 1, 2, 1, 0},
+        {0, 0, 0, 2, 2, 0, 2, 0, 2, 2, 0, 0, 0}
+    };
+
+    // zemin ve duvar
+    sf::RectangleShape kare(sf::Vector2f(kare_boyutu, kare_boyutu));
+    kare.setOutlineThickness(-1.f);
+    kare.setOutlineColor(sf::Color(50, 50, 50));
+
+    // oyuncu karakteri
+    sf::CircleShape oyuncu(kare_boyutu / 2.5f);
+    oyuncu.setFillColor(sf::Color::Blue);
+    oyuncu.setOrigin(sf::Vector2f(oyuncu.getRadius(), oyuncu.getRadius()));
+
+    // oyuncu baþlangýç yeri
+    sf::Vector2f oyuncuKonum(kare_boyutu / 2.f, kare_boyutu / 2.f);
+    float hiz = 4.0f;
+
+    // döngü
+    while (pencere.isOpen()) {
+        
+        while (const std::optional<sf::Event> olay = pencere.pollEvent()) {
+            if (olay->is<sf::Event::Closed>())
+                pencere.close();
+        }
+
+        // hareket þekli
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) oyuncuKonum.y -= hiz;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) oyuncuKonum.y += hiz;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) oyuncuKonum.x -= hiz;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) oyuncuKonum.x += hiz;
+
+        oyuncu.setPosition(oyuncuKonum);
+
+        pencere.clear();
+
+        // harita
+        for (unsigned int i = 0; i < satir_sayisi; ++i) {
+            for (unsigned int j = 0; j < sutun_sayisi; ++j) {
+                kare.setPosition(sf::Vector2f(j * kare_boyutu, i * kare_boyutu));
+
+                if (harita[i][j] == KIRILAMAZ_DUVAR)
+                    kare.setFillColor(sf::Color(100, 100, 100)); // gri
+                else if (harita[i][j] == KIRILABILIR_KUTU)
+                    kare.setFillColor(sf::Color(200, 100, 50));  // kahverengi
+                else
+                    kare.setFillColor(sf::Color(34, 139, 34));   // yeþil
+
+                pencere.draw(kare);
+            }
+        }
+
+        // oyuncuyu çiz
+        pencere.draw(oyuncu);
+
+        pencere.display();
+    }
 
     return 0;
 }
